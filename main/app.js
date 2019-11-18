0c1223b5a05d09e1c0f42ca849e727b91340d855
Ubuntu <ubuntu@ip-172-31-34-4.us-east-2.compute.internal>
2019-11-18 18:23:23 +0000

    Tema2

--- main/app.js
+++ main/app.js
@@ -1,9 +1,21 @@
 function addTokens(input, tokens){
-    
-}
-
+    if(typeof input!='string'){
+        throw new Error('Invalid input')
+    }
+    if(input.length<6){
+        throw new Error( 'Input should have at least 6 characters')
+    }
+    for(var k in tokens){
+        if(typeof tokens[k].tokenName!=='string')
+       { throw new Error('Invalid array format')}
+        if(input.includes('...')){
+        input=input.replace('...','${'+tokens[k].tokenName+'}');
+        return input;}
+        else{
+            return input;
+        }
+    }}
 const app = {
     addTokens: addTokens
 }
-
 module.exports = app;
